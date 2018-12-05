a38c27e5f46e42385d7a6e5047baa8ac03dfae41
IPanhui <panhui@live.cn>
2018-12-05 20:11:42 +0800

    sdaf

--- learn/pachong/scraping.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-url = "http://www.pythonscraping.com/pages/page1.html"
-html = urlopen(url)
-bsObj = BeautifulSoup(html.read())
-h1 = bsObj.html.body.h1
-print(h1)
-
